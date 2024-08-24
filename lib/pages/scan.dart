8d7948086f6da2c1076a504389ae8fd3f0a3f1ca
brentibanez331 <94149036+brentibanez331@users.noreply.github.com>
2024-08-24 23:38:14 +0800

    :rocket: card updates, katugnaw di sakon ginatiniran

--- lib/pages/scan.dart
+++ lib/pages/scan.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:thesis/pages/scan_result.dart';
 import 'package:thesis/services/camera_service.dart';
-import 'package:thesis/utils/containerWidgets.dart';
+import 'package:thesis/utils/colors.dart';
+import 'package:thesis/widgets/containerWidgets.dart';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({Key? key}) : super(key: key);
@@ -50,141 +51,230 @@ class _ScanPageState extends State<ScanPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Scan Page'),
-      ),
+      // appBar: AppBar(
+      //   title: const Text('Scan Page'),
+      // ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: _openCamera,
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        foregroundColor: Colors.white,
-                        minimumSize: const Size(20, 100),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(40))),
-                    child: const Text(
-                      "Open Camera",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 10), // Add some space between the buttons
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: _openGallery,
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        foregroundColor: Colors.white,
-                        minimumSize: const Size(20, 100),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(40))),
-                    child: const Text(
-                      "Upload Image",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            if (image != null)
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
               Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: Image.file(image!),
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: 100,
+                        child: ElevatedButton(
+                          onPressed: _openCamera,
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue.withOpacity(0.2),
+                              foregroundColor: Colors.white,
+                              // minimumSize: const Size(20, 100),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(32))),
+                          child: const Text(
+                            "Open Camera",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                        width: 10), // Add some space between the buttons
+                    Expanded(
+                      child: SizedBox(
+                        height: 100,
+                        child: ElevatedButton(
+                          onPressed: _openGallery,
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue.withOpacity(0.2),
+                              foregroundColor: Colors.white,
+                              // minimumSize: const Size(20, 100),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(32))),
+                          child: const Text(
+                            "Upload Image",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            const SizedBox(
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: EdgeInsets.only(top: 50.0),
-                  child: Text(
-                    "Newly Saved Images",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              if (image != null)
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Image.file(image!),
+                ),
+              const SizedBox(
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: Text(
+                      "Recent Scans",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
                   ),
                 ),
               ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  Containerwidgets().historyButton(
-                      context,
-                      'img/leafSample.jpeg',
-                      'Tomato Blight',
-                      'Tomato',
-                      160,
-                      200),
-                  Containerwidgets().historyButton(
-                      context,
-                      'img/leafSample.jpeg',
-                      'Banana Blight',
-                      'Tomato',
-                      160,
-                      200),
-                  Containerwidgets().historyButton(
-                      context,
-                      'img/leafSample.jpeg',
-                      'Grape Blight',
-                      'Tomato',
-                      160,
-                      200),
-                  Containerwidgets().historyButton(context,
-                      'img/leafSample.jpeg', 'Apple Blight', 'Tomato', 160, 200)
-                ],
+              SizedBox(
+                height: 200,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: buttonTitles.length,
+                  itemBuilder: (context, index) {
+                    return SizedBox(
+                      width: 200,
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image(
+                                image: AssetImage("img/leafSample.jpeg"),
+                                height: 100,
+                                width: double.maxFinite,
+                                fit: BoxFit.cover,
+                              ),
+                              SizedBox(height: 10),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(buttonTitles[index]),
+                                    Text("Status: "),
+                                    Text("Aug 28, 10:00 AM"),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        color: AppColors.primary,
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-            const SizedBox(
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: EdgeInsets.only(top: 40.0),
-                  child: Text(
-                    "Previous Saved Images",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+
+              // SingleChildScrollView(
+              //   scrollDirection: Axis.horizontal,
+              //   child: Row(
+              //     children: [
+              //       Containerwidgets().historyButton(
+              //           context,
+              //           'img/leafSample.jpeg',
+              //           'Tomato Blight',
+              //           'Tomato',
+              //           160,
+              //           200),
+              //       Containerwidgets().historyButton(
+              //           context,
+              //           'img/leafSample.jpeg',
+              //           'Banana Blight',
+              //           'Tomato',
+              //           160,
+              //           200),
+              //       Containerwidgets().historyButton(
+              //           context,
+              //           'img/leafSample.jpeg',
+              //           'Grape Blight',
+              //           'Tomato',
+              //           160,
+              //           200),
+              //       Containerwidgets().historyButton(
+              //           context,
+              //           'img/leafSample.jpeg',
+              //           'Apple Blight',
+              //           'Tomato',
+              //           160,
+              //           200)
+              //     ],
+              //   ),
+              // ),
+              SizedBox(
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "Previous 30 Days",
+                              // style:
+                              //     TextStyle(fontSize: 20),
+                            ),
+                            SizedBox(width: 10),
+                            Icon(Icons.info_outline)
+                          ],
+                        ),
+                        TextButton(
+                          child: Text("View All"),
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  Containerwidgets().historyButton(
-                      context,
-                      'img/leafSample.jpeg',
-                      'Tomato Blight',
-                      'Tomato',
-                      120,
-                      160),
-                  Containerwidgets().historyButton(
-                      context,
-                      'img/leafSample.jpeg',
-                      'Banana Blight',
-                      'Tomato',
-                      120,
-                      160),
-                  Containerwidgets().historyButton(
-                      context,
-                      'img/leafSample.jpeg',
-                      'Grape Blight',
-                      'Tomato',
-                      120,
-                      160),
-                  Containerwidgets().historyButton(context,
-                      'img/leafSample.jpeg', 'Apple Blight', 'Tomato', 120, 160)
-                ],
-              ),
-            ),
-          ],
+              ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: buttonTitles.length,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                        padding: const EdgeInsets.only(bottom: 4.0),
+                        child: Card(
+                          // style: FilledButton.styleFrom(
+                          //     backgroundColor: AppColors.cardBackground),
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Image(
+                                          image:
+                                              AssetImage("img/leafSample.jpeg"),
+                                          width: 50,
+                                          height: 50),
+                                      SizedBox(width: 10),
+                                      Column(
+                                        // mainAxisAlignment: MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(buttonTitles[index]),
+                                          Text("Status: Blight"),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  Text("Aug 24")
+                                ],
+                              ),
+                            ),
+                          ),
+                        ));
+                  })
+            ],
+          ),
         ),
       ),
     );
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static Color primary = Colors.white;
   static Color secondary = Colors.black;
   static Color buttonBackground = Colors.blue;
+  static Color cardBackground = Colors.white;
 }
