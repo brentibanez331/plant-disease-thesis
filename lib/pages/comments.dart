9e0f4bb38e14eaf2ddf2ed0130e5a6b7dd24e121
PatrickJohn243 <florespatrickjohn243@gmail.com>
2024-11-12 11:00:23 +0800

    fix: post directs to comments on click

--- lib/pages/comments.dart
+++ lib/pages/comments.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:thesis/models/post.dart';
+import 'package:thesis/models/user.dart';
 
 class Comments extends StatefulWidget {
-  const Comments({super.key});
+  // final UserModel user;
+  // final Post post;
+  const Comments({
+    super.key,
+    // required this.user,
+    // required this.post,
+  });
 
   @override
   _CommentsState createState() => _CommentsState();
@@ -13,227 +21,220 @@ class _CommentsState extends State<Comments> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('FOO'),
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.blue,
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 15.0, top: 15, right: 15),
-              child: Row(
-                children: [
-                  const CircleAvatar(
-                    radius: 15,
-                    backgroundImage:
-                        NetworkImage('https://via.placeholder.com/40x40'),
-                  ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'AccountName',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        '1d ⋅ 12k views',
-                        style: TextStyle(color: Colors.grey[600], fontSize: 12),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'LOREM IPSUM',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  maxLines: 3,
-                  overflow: TextOverflow.ellipsis,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Post'),
+        foregroundColor: Colors.black,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15),
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  radius: 15,
+                  backgroundImage:
+                      NetworkImage('https://via.placeholder.com/40x40'),
                 ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'AccountName',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '1d ⋅ 12k views',
+                      style: TextStyle(color: Colors.grey[600], fontSize: 12),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'LOREM IPSUM',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                    maxLines: isExpanded ? null : 3,
-                    overflow: isExpanded
-                        ? TextOverflow.visible
-                        : TextOverflow.ellipsis,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                  maxLines: isExpanded ? null : 3,
+                  overflow:
+                      isExpanded ? TextOverflow.visible : TextOverflow.ellipsis,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isExpanded = !isExpanded;
+                    });
+                  },
+                  child: Text(
+                    isExpanded ? 'See less' : 'See more',
+                    style: TextStyle(color: Colors.grey[600]),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        isExpanded = !isExpanded;
-                      });
-                    },
-                    child: Text(
-                      isExpanded ? 'See less' : 'See more',
-                      style: TextStyle(color: Colors.grey[600]),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 15, top: 12.0, bottom: 12, right: 15),
-              child: Row(
-                children: [
-                  Container(
-                    height: 32,
-                    decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      borderRadius: BorderRadius.circular(16),
-                      border: Border.all(
-                        color: Colors.grey[300]!,
-                        width: 1,
-                      ),
-                    ),
-                    child: Row(
-                      children: [
-                        // Like Button
-                        SizedBox(
-                          height: 32,
-                          child: InkWell(
-                            borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(16),
-                              bottomLeft: Radius.circular(16),
-                            ),
-                            onTap: () {
-                              setState(() {
-                                isLiked = !isLiked;
-                              });
-                            },
-                            child: Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 12.0),
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    isLiked
-                                        ? Icons.thumb_up_alt
-                                        : Icons.thumb_up_alt_outlined,
-                                    size: 18,
-                                  ),
-                                  const SizedBox(width: 4),
-                                  Text(
-                                    '100',
-                                    style: TextStyle(
-                                        color: Colors.grey[600], fontSize: 13),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          height: 20,
-                          width: 1,
-                          color: Colors.grey[300],
-                        ),
-                        // Comment Button
-                        SizedBox(
-                          height: 32,
-                          child: InkWell(
-                            borderRadius: const BorderRadius.only(
-                              topRight: Radius.circular(16),
-                              bottomRight: Radius.circular(16),
-                            ),
-                            onTap: () {},
-                            child: Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 12.0),
-                              child: Row(
-                                children: [
-                                  const Icon(Icons.comment, size: 18),
-                                  const SizedBox(width: 4),
-                                  Text(
-                                    '49',
-                                    style: TextStyle(
-                                        color: Colors.grey[600], fontSize: 13),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 15, top: 12.0, bottom: 12, right: 15),
+            child: Row(
+              children: [
+                Container(
+                  height: 32,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(
+                      color: Colors.grey[300]!,
+                      width: 1,
                     ),
                   ),
-                  const SizedBox(width: 20),
-                  // Share Button
-                  Container(
-                    height: 32,
-                    decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      borderRadius: BorderRadius.circular(16),
-                      border: Border.all(
-                        color: Colors.grey[300]!,
-                        width: 1,
-                      ),
-                    ),
-                    child: InkWell(
-                      borderRadius: BorderRadius.circular(16.0),
-                      onTap: () {
-                        print('Share button pressed');
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                        child: Row(
-                          children: [
-                            const Icon(Icons.share, size: 18),
-                            const SizedBox(width: 4),
-                            Text(
-                              'Share',
-                              style: TextStyle(
-                                color: Colors.grey[600],
-                                fontSize: 13,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              height: 10,
-              width: double.infinity,
-              color: Colors.grey[300],
-            ),
-            // Wrap ListView.builder with Expanded
-            Expanded(
-              child: ListView.builder(
-                itemCount: 10, // Limit the number of items to display
-                itemBuilder: (BuildContext context, int index) {
-                  return const Column(
+                  child: Row(
                     children: [
-                      Padding(padding: EdgeInsets.all(8)),
-                      Text('halo')
+                      // Like Button
+                      SizedBox(
+                        height: 32,
+                        child: InkWell(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(16),
+                            bottomLeft: Radius.circular(16),
+                          ),
+                          onTap: () {
+                            setState(() {
+                              isLiked = !isLiked;
+                            });
+                          },
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  isLiked
+                                      ? Icons.thumb_up_alt
+                                      : Icons.thumb_up_alt_outlined,
+                                  size: 18,
+                                ),
+                                const SizedBox(width: 4),
+                                Text(
+                                  '100',
+                                  style: TextStyle(
+                                      color: Colors.grey[600], fontSize: 13),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 20,
+                        width: 1,
+                        color: Colors.grey[300],
+                      ),
+                      // Comment Button
+                      SizedBox(
+                        height: 32,
+                        child: InkWell(
+                          borderRadius: const BorderRadius.only(
+                            topRight: Radius.circular(16),
+                            bottomRight: Radius.circular(16),
+                          ),
+                          onTap: () {},
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: Row(
+                              children: [
+                                const Icon(Icons.comment, size: 18),
+                                const SizedBox(width: 4),
+                                Text(
+                                  '49',
+                                  style: TextStyle(
+                                      color: Colors.grey[600], fontSize: 13),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
                     ],
-                  );
-                },
-              ),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                // Share Button
+                Container(
+                  height: 32,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(
+                      color: Colors.grey[300]!,
+                      width: 1,
+                    ),
+                  ),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(16.0),
+                    onTap: () {
+                      print('Share button pressed');
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: Row(
+                        children: [
+                          const Icon(Icons.share, size: 18),
+                          const SizedBox(width: 4),
+                          Text(
+                            'Share',
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Container(
+            height: 10,
+            width: double.infinity,
+            color: Colors.grey[300],
+          ),
+          // Wrap ListView.builder with Expanded
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10, // Limit the number of items to display
+              itemBuilder: (BuildContext context, int index) {
+                return const Column(
+                  children: [Padding(padding: EdgeInsets.all(8)), Text('halo')],
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
